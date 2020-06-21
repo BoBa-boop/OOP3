d472584b97b352fb01124d0d6f47e74f83509af3
Никита Федосеев <zveri132@gmail.com>
2020-06-21 19:49:04 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Book
+    {
+        private string Author;
+        private string Nazv;
+        private int Str;
+        private bool Kol;
+        public Book(string Author, string Nazv, int Str, bool Kol)
+        {
+            this.Author = Author;
+            this.Nazv = Nazv;
+            this.Str = Str;
+            this.Kol = Kol;
+        }
+        public void Print()
+
+        {
+            Console.WriteLine("Автор - " + Author);
+            Console.WriteLine("Название книги - " + Nazv);
+            Console.WriteLine("Количество страниц - " + Str);
+            Console.WriteLine("Коллекционное издание - " + Kol);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Book nBook = new Book("Пушкин А.С.", "Евгений Онеген", 400, true);
+                nBook.Print();
+                Console.ReadKey();
+        }
+
+    }
+}
